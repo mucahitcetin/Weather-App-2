ebcdaa806cf8a10642f5a4792757e4fff2040cca
mucahitcetin <mucahitcetin01@gmail.com>
2024-03-11 16:57:50 +0300

    first commit

---
+++ js/api.js
@@ -0,0 +1,21 @@
+const API_KEY = "1e24c1fd664a14ac1dca15d3ec0ea124";
+
+// fonksiyon şehir ve birim bilgilerini parametre olarak alır
+async function fetchWeatherData(city, units) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`
+    );
+    // console.log(response.json());
+    // eğer istek başarılı değilse hata fırlat
+    if (!response.ok) {
+      throw new Error("İstek başarılı değil");
+    }
+    // api'den gelen yanıtı JSON formatına dönüştür ve geri dönder
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+// fetchWeatherData fonksiyonunu diğer dosyalarda kullanılabilir hale getir.
+export default fetchWeatherData;
